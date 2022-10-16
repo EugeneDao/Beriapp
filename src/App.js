bfa0e392bb61e85b82470841de1ee13ce157d0f2
unknown <phuclethinh70@gmail.com>
2022-10-16 11:50:47 +0700

    addlandingpage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import React, { useState } from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
-
+import Home from './pages/Home';
+import { BrowserRouter as Router, Switch, Route, useNavigate, Routes } from 'react-router-dom';
+import Services from './pages/Services';
+import Products from './pages/Products';
+import SignUp from './pages/SignUp';
+import NavbarMain from './ComponentsMain/NavbarMain';
 function App() {
   return (
-    <div>
-      <Chartpage/>
-      <Portfolio/>
-    </div>
+    <>
+      <Router>
+
+        {/* <Navbar/> */}
+        <Routes>
+          <Route path='/' exact element={<Home />} />
+          <Route path='/beri' element={<NavbarMain />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/sign-up' element={<SignUp />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
 export default App;
-``
---
+++ src/pages/Services.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../App.css';
+import Navbar from '../components/Navbar';
+export default function Services() {
+  return <div>
+    <Navbar/>
+    <h1 className='services'>SERVICES</h1>;</div>
+}
