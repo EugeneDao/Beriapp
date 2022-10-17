6158956b58644748bae107f29c6a09206abf8e58
EugeneDao <hoangkhang0209@gmail.com>
2022-10-17 20:27:52 +0700

    facebooklogin

--- src/LandingPage/components/config.js
+++ src/LandingPage/components/config.js
@@ -3,8 +3,6 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import 'firebase/compat/analytics';
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,9 +19,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = firebase.auth();
-const db = firebase.firestore();
-export {db, auth};
+firebase.initializeApp(firebaseConfig);
+export const auth = firebase.auth();
+export const db = firebase.firestore();
 export default firebase;
