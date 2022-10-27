5ab91f6ab91555884307a323d44e3334ffb07662
ttluan97ccnc <ttluan97ccnc@gmail.com>
2022-10-27 18:14:36 +0700

    test git

--- src/App.js
+++ src/App.js
@@ -6,21 +6,12 @@ import Services from './LandingPage/pages/Services'
 import Products from './LandingPage/pages/Products'
 import SignUp from './LandingPage/pages/SignUp'
 import { BrowserRouter as Router, Switch, Route, useNavigate, Routes, Form } from 'react-router-dom';
+import AppContainer from './Web App Components/AppContainer/AppContainer'
 
 function App() {
   return (
     <div>
-     <Router>
-
-{/* <Navbar/> */}
-<Routes>
-  <Route path='/' exact element={<Home />} />
-  <Route path='/beri' element={<NavbarMain />} />
-  <Route path='/services' element={<Services />} />
-  <Route path='/products' element={<Products />} />
-  <Route path='/sign-up' element={<SignUp />} />
-</Routes>
-</Router>
+      <AppContainer/>
     </div>
   );
 }
